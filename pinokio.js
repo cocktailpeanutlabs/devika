7388c3285a983f2fcd3519fe42f5ad489fe2f2db
cocktailpeanut <cocktailpeanuts@proton.me>
2024-06-18 23:37:39 -0400

    update

--- pinokio.js
+++ pinokio.js
@@ -22,6 +22,7 @@ module.exports = {
             icon: "fa-solid fa-rocket",
             text: "Open Web UI",
             href: local.url,
+            popout: true
           }, {
             icon: 'fa-solid fa-terminal',
             text: "Terminal",
