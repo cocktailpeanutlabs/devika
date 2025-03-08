f854ab44a3428d2b76961c7634e7ae16a9cdaa07
6Morpheus6 <wanton-decay@gmx.net>
2025-03-08 20:23:03 +0100

    revert

--- install.js
+++ install.js
@@ -3,7 +3,7 @@ module.exports = {
     method: "shell.run",
     params: {
       message: [
-        "git clone https://github.com/peanutcocktail/devika app",
+        "git clone https://github.com/6Morpheus6/devika app",
       ]
     }
   }, {
